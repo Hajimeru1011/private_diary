fd5085728ec533793bada3f830f90740d98300d1
Hajimeru <hmiyake1005@gmail.com>
2020-10-18 20:13:39 +0200

    accounts etc..., are added.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
 
-from .models import Diary
-
-admin.site.register(Diary)
+admin.site.register(CustomUser)
